8c737e727d0faa2887741cda82a64cff34a4c71a
xavier <XAVIER.HUANG@OOCL.COM>
2025-09-05 15:35:51 +0800

    feat: test input_command_M_direction_N and input_command_M_Direction_E

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,33 @@
+package com.afs.tdd;
+
+public class MarsRover {
+    private int x;
+    private int y;
+    private String direction;
+    public MarsRover(int x, int y, String direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+
+    public void executeCommand(String command) {
+        if(direction.equals("N")) {
+            y++;
+        }
+        if(direction.equals("E")) {
+            x++;
+        }
+
+    }
+}
---
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -0,0 +1,29 @@
+package com.afs.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MarsRoverTest {
+    @Test
+    public void should_return_y_plus_when_input_command_M_direction_N(){
+        String command = "M";
+
+        MarsRover marsRover = new MarsRover(0,0,"N");
+        marsRover.executeCommand(command);
+
+        assertEquals(1,marsRover.getY());
+    }
+
+    @Test
+    public void should_return_y_plus_when_input_command_M_Direction_E(){
+        String command = "M";
+
+        MarsRover marsRover = new MarsRover(0,0,"E");
+        marsRover.executeCommand(command);
+
+        assertEquals(1,marsRover.getX());
+    }
+
+
+}
