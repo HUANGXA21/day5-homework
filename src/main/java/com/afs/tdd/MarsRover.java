8f86f300fc3ea74d051e33178a9c540567d48f26
xavier <XAVIER.HUANG@OOCL.COM>
2025-09-05 16:24:29 +0800

    refactor: format code

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,9 +1,11 @@
 package com.afs.tdd;
 
 public class MarsRover {
+
     public static final String MOVE = "M";
     public static final String LEFT = "L";
     public static final String RIGHT = "R";
+
     public static final String NORTH = "N";
     public static final String EAST = "E";
     public static final String SOUTHERN = "S";
